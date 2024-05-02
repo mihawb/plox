94ed71080932b2644dabe5008fe03e6e438e22f4
mihawb <michalbanaszczak@gmail.com>
2024-05-02 17:09:56 +0200

    5.3 visitor pattern or AST

--- lox/expressions.py
+++ lox/expressions.py
@@ -3,8 +3,16 @@ from typing import Any
 from tokens import Token
 
 
+class Visitor:
+    def visit_binary(self, expr): pass
+    def visit_grouping(self, expr): pass
+    def visit_literal(self, expr): pass
+    def visit_unary(self, expr): pass
+
+
 class Expr:
-    pass
+    def accept(self, visitor: Visitor):
+        pass
 
 
 @dataclass
@@ -13,18 +21,30 @@ class Binary(Expr):
     operator: Token
     right: Expr
 
+    def accept(self, visitor: Visitor):
+        return visitor.visit_binary(self)
+
 
 @dataclass
 class Grouping(Expr):
     expression: Expr
+
+    def accept(self, visitor: Visitor):
+        return visitor.visit_grouping(self)
 
 
 @dataclass
 class Literal(Expr):
     value: Any
 
+    def accept(self, visitor: Visitor):
+        return visitor.visit_literal(self)
+
 
 @dataclass
 class Unary(Expr):
     operator: Token
     right: Expr
+
+    def accept(self, visitor: Visitor):
+        return visitor.visit_unary(self)
--- tool/generate_ast.py
+++ tool/generate_ast.py
@@ -2,6 +2,8 @@ import sys
 
 # istg I need to learn macros in Rust
 
+# 5.3, 5.3.1 minimal explanation how functional PLs would approach the expression problem with pattern matching
+
 
 NLNL = "\n\n"
 TAB = "    "  # 4 spaces in lieu of \t
@@ -29,6 +31,9 @@ def generate_expr_meta_dataclass(expr_def: str) -> str:
     for field in fields:
         meta_class_def += f"{TAB}{field}\n"
 
+    meta_class_def += f"\n{TAB}def accept(self, visitor: Visitor):\n"
+    meta_class_def += f"{TAB * 2}return visitor.visit_{class_name.lower()}(self)\n"
+
     return meta_class_def
 
 
@@ -38,11 +43,23 @@ if __name__ == "__main__":
         sys.exit(64)
 
     with open(sys.argv[1], "w") as ast_file:
-
+        # imports
         ast_file.writelines(map(lambda i: i + "\n", IMPORTS))
-        ast_file.write(NLNL)
-        ast_file.write(f"class Expr:\n{TAB}pass\n")
 
+        # Visitor interface definition
+        ast_file.write(NLNL)
+        ast_file.write("class Visitor:\n")
+        for expr in EXPRS:
+            class_name = expr.split("->")[0].strip()
+            ast_file.write(f"{TAB}def visit_{class_name.lower()}(self, expr): pass\n")
+
+        # Expr definition
+        ast_file.write(NLNL)
+        ast_file.write("class Expr:\n")
+        ast_file.write(f"{TAB}def accept(self, visitor: Visitor):\n")
+        ast_file.write(f"{TAB * 2}pass\n")
+
+        # Expr subclasses definitions
         for expr in EXPRS:
             ast_file.write(NLNL)
             meta_class_def = generate_expr_meta_dataclass(expr)
